efa6582a8cf5bbe5bf3a9f3b7826918c5a766bb4
rock-mak <makwahlok@gmail.com>
2025-03-12 11:57:04 +0800

    Update 31: Guess Letters.py

--- 31: Guess Letters.py
+++ 31: Guess Letters.py
@@ -1,49 +1,44 @@
 import random
 hangman_file = open('sowpods.txt')
-target = random.choice(hangman_file.readlines())
+words = hangman_file.readlines()
 
 def word_len(word):
-    return "_ " * (len(word)-1)
+    return "_ " * (len(word))
 
 def hangman(guess,word):
-    word = enumerate(word)
-    num_list = []
-    for i,v in word:
-        if guess in v:
-            num_list.append(i)
-    return num_list
+    return [i for i,v in enumerate(word) if v == guess]
             
 playing = True
-count = 6
+
 print('Welcome to hangman!')
-print(target)
-show = word_len(target)
 
 while playing:
-    if count == 0:
-        print(f"Chance left: {count}\nBetter luck next time!")
-        break
-    print(show, "  Guess chances: ", count)
-    user_ = input("Guess your letter: ")
-    
-    if user_.isalpha() and len(user_) == 1:
-        user_ = user_.upper()
-        if user_ in target:
-            for i in hangman(user_,target):
-                show = list(show.replace(' ',''))
-                show[int(i)] = user_
-                show = ' '.join(show)
+    count = 6
+    target = random.choice(words).strip().upper()
+    show = word_len(target).strip()
+    print(target)
+    while count > 0:
+        print(show, "  Guess chances: ", count)
+        user_ = input("Guess your letter: ")
+        if user_.isalpha() and len(user_) == 1:
+            user_ = user_.upper()
+            if user_ in target:
+                for i in hangman(user_,target):
+                    show_list = list(show.replace(' ',''))
+                    show_list[i] = user_
+                    show = ' '.join(show_list)
                 if show.replace(" ",'') == target:
                     print(f"Congratulation! Tries left: {count}")
-                    playing = False
+                    break
+            else:
+                count -= 1
+                print("Incorrect!")
         else:
-            count -= 1
-            print("Incorrect!")
-    else:
-        print("Invalid entry!")
+            print("Invalid entry!")
+    if count == 0:
+            print(f"Chance left: {count}\nBetter luck next time!\nThe word is {target}")
     
-again = input("Do you want to play again?")
-if "YES" or 'yes' in input:
-    playing = True
-else:
-    print("Thank you for playing!")
+    again = input("Do you want to play again?")
+    if "YES" or 'yes' not in input:
+        print("Thank you for playing!")
+        playing = False
