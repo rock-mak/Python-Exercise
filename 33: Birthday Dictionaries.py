e5f10cdf966fbf1b2d8080dfd4c6a6eb33d3a858
rock-mak <makwahlok@gmail.com>
2025-03-12 17:44:07 +0800

    Update 33: Birthday Dictionaries.py

--- 33: Birthday Dictionaries.py
+++ 33: Birthday Dictionaries.py
@@ -1,21 +1,38 @@
 import datetime
+import string
 
-birthday = {}
-
-def add_ele(dic, name, b_day):
-    dic[name] = b_day
+birthday = {
+    'apple fruit':(datetime.datetime())
+}
 
 print("Welcome to the birthday dictionary. We know the birthdays of:")
-try:
-    for key, value in birthday:
-        print(key)
-except ValueError:
-    return None
+for items in birthday.keys():
+    print(string.capwords(items))
 while True:
-    ask = input("Do you want to look up or input? ")
-    if "input" in ask.lower():
-        name = input("What is your name? ")
-        b_day = input("What is your day of birth? ")
-        add_ele(birthday, name, b_day)
-    elif "look up" in ask.lower():
-        lookup = input
+    ask = input("Do you want to search or input?")
+    if ask.isalpha():
+        ask = ask.lower()
+        if "input" in ask:
+            name = input("What is your name? ")
+            DD = int(input("Please enter your day of birth accordingly?\nDate: "))
+            MM = int(input("Month: "))
+            YY = int(input("Year: "))
+            try:
+                b_day = datetime.datetime(YY,MM,DD)
+                birthday[(name.lower())] = b_day
+            except TypeError:
+                print("Invalid entry!")
+        elif "search" in ask:
+            search = input("Who's birthday do you want to look up?")
+            if search in birthday:
+                bday = birthday[search]
+                lDD = bday.strftime("%d")
+                lMM = bday.strftime("%m")
+                lYY = bday.strftime("%Y")
+                print(f"{string.capwords(search)}'s birthday is bday {lDD}/{lMM}/{lYY} ")
+        elif "exit" in ask:
+            break
+        else:
+            print("Invalid entry!")
+    else:
+        print("Invalid entry")
