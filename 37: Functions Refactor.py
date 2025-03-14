bda34c83870c0e5ff78ab3928add8fb05707d311
rock-mak <makwahlok@gmail.com>
2025-03-14 16:45:39 +0800

    Update 37: Functions Refactor.py

--- 37: Functions Refactor.py
+++ 37: Functions Refactor.py
@@ -1,11 +1,14 @@
 def board(num):
     top = " ----" 
     mid = ("|    ")
-
+    matrix = []
     for i in range(num):
-        print(top*num)
-        print((mid*num)+'|')
-    print(top*num)
+        matrix.append(top*num)
+        matrix.append((mid*num)+'|')
+    matrix.append(top*num)
+    return matrix
     
 ask = int(input("What size do you want? "))
 board = board(ask)
+for i in board:
+    print(i)
